fdbffe119f74f4d9562da07494fe73daf29c6f81
Adrien <adrien.moncieu@gmail.com>
2022-10-22 15:21:30 +0200

    maj

--- src/Controller/TodoController.php
+++ src/Controller/TodoController.php
@@ -20,10 +20,10 @@ class TodoController extends AbstractController
         // Afficher notre tableau de todo
         // sinon je l'initialise puis j'affiche
         if (!$session->has('todos')) {
-            $todos =[
-                'achat'=>'acheter clé usb',
-                'cours'=>'Finaliser mon cours',
-                'correction'=>'corriger mes examens'
+            $todos = [
+                'achat' => 'acheter clé usb',
+                'cours' => 'Finaliser mon cours',
+                'correction' => 'corriger mes examens'
             ];
             $session->set('todos', $todos);
             $this->addFlash('info', "La liste des todos viens d'être initialisée");
@@ -32,10 +32,11 @@ class TodoController extends AbstractController
         return $this->render('todo/index.html.twig');
     }
     #[Route(
-        '/todo/add/{name}/{content}',
+        '/add/{name?test}/{content?test}',
         name: 'todo.add'
     )]
-    public function addTodo(Request $request, $name, $content): RedirectResponse {
+    public function addTodo(Request $request, $name, $content): RedirectResponse
+    {
         $session = $request->getSession();
         // Vérifier si j ai mon tableau de todo dans la session
         if ($session->has('todos')) {
@@ -58,8 +59,61 @@ class TodoController extends AbstractController
         }
         return $this->redirectToRoute('todo');
     }
-
+    #[Route('/update/{name}/{content}', name: 'todo.update')]
+    public function updateTodo(Request $request, $name, $content): RedirectResponse
+    {
+        $session = $request->getSession();
+        // Vérifier si j ai mon tableau de todo dans la session
+        if ($session->has('todos')) {
+            // si oui
+            // Vérifier si on a déjà un todd avec le meme name
+            $todos = $session->get('todos');
+            if (!isset($todos[$name])) {
+                // si oui afficher errerur
+                $this->addFlash('error', "Le todo d'id $name n'existe pas dans la liste");
+            } else {
+                // si non on l'ajouter et on affiche un message de succès
+                $todos[$name] = $content;
+                $session->set('todos', $todos);
+                $this->addFlash('success', "Le todo d'id $name a été modifié avec succès");
+            }
+        } else {
+            // si non
+            // afficher une erreur et on va redirger vers le controlleur index
+            $this->addFlash('error', "La liste des todos n'est pas encore initialisée");
+        }
+        return $this->redirectToRoute('todo');
+    }
+    #[Route('/delete/{name}', name: 'todo.delete')]
+    public function deleteTodo(Request $request, $name): RedirectResponse
+    {
+        $session = $request->getSession();
+        // Vérifier si j ai mon tableau de todo dans la session
+        if ($session->has('todos')) {
+            // si oui
+            // Vérifier si on a déjà un todd avec le meme name
+            $todos = $session->get('todos');
+            if (!isset($todos[$name])) {
+                // si oui afficher errerur
+                $this->addFlash('error', "Le todo d'id $name n'existe pas dans la liste");
+            } else {
+                // si non on l'ajouter et on affiche un message de succès
+                unset($todos[$name]);
+                $session->set('todos', $todos);
+                $this->addFlash('success', "Le todo d'id $name a été supprimé avec succès");
+            }
+        } else {
+            // si non
+            // afficher une erreur et on va redirger vers le controlleur index
+            $this->addFlash('error', "La liste des todos n'est pas encore initialisée");
+        }
+        return $this->redirectToRoute('todo');
+    }
+    #[Route('/reset', name: 'todo.reset')]
+    public function resetTodo(Request $request): RedirectResponse
+    {
+        $session = $request->getSession();
+        $session->remove('todos');
+        return $this->redirectToRoute('todo');
+    }
 }
-
-
-// againgit 
