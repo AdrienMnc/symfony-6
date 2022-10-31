8c6ddd41283063329fe23fcabeca2ebe21dafa12
Adrien <adrien.moncieu@gmail.com>
2022-10-31 08:26:50 +0100

    test twig

--- src/Controller/FirstController.php
+++ src/Controller/FirstController.php
@@ -4,15 +4,36 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class FirstController extends AbstractController
 {
     #[Route('/first', name: 'app_first')]
-    public function index(): Response
+    public function template(): Response
     {
         return $this->render('first/index.html.twig', [
             'controller_name' => 'FirstController',
+        ]);
+    }
+
+
+    #[Route('/first', name: 'first')]
+    public function index(): Response
+    {
+        // chercher au la abse de données vos users
+        return $this->render('first/index.html.twig', [
+            'name' => 'Mnc',
+            'firstname' => 'Adri'
+        ]);
+    }
+
+    //    #[Route('/sayHello/{name}/{firstname}', name: 'say.hello')]
+    public function sayHello(Request $request, $name, $firstname): Response
+    {
+        return $this->render('first/index.html.twig', [
+            'nom' => $name,
+            'prenom' => $firstname
         ]);
     }
 }
